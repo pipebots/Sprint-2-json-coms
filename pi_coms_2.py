6f8536260a638193dc4229dd0b3502195086e171
nfry321 <58332182+nfry321@users.noreply.github.com>
2020-03-10 17:46:06 +0000

    edited comments

--- pi_coms_2.py
+++ pi_coms_2.py
@@ -2,15 +2,15 @@ import serial
 import json
 import time
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600) #address when using USB cable to arduino
 ser.flushInput()
 
 # function to read JSON doc from serial and parse variables from it
 def listenJSON():
     try:
-        readStr = ser.readline()
+        readStr = ser.readline() #doc is sent with line end termination
         #print(readStr)
-        readJson = json.loads(readStr)
+        readJson = json.loads(readStr) 
         #print(readJson)
         dist = readJson["Colour"]
         imu = readJson["imu"]
@@ -19,14 +19,14 @@ def listenJSON():
         yaw = imu[2]
         print(dist,roll,pitch,yaw,)
     except:
-        print("not valid json") #This usually only happens once at the start
+        print("not valid json") #At the start it is common for a partial JSON doc to be recieved and throw an error - this ignores it
         pass
 
 #function to send data formatted as JSON doc over serial
 def writeJSON(led):
     sendStr = json.dumps({'LED':led}, separators=(',',':')) + "\n"
     #print(sendStr)
-    ser.write(sendStr.encode('utf-8')) #has to be encoded into bits
+    ser.write(sendStr.encode('utf-8')) #has to be encoded into bits for .write
 
 i=0
 while True:
