ac90f49f025c0d2284eac000c8c10e4d92b2d6e9
nfry321 <nickfry321@gmail.com>
2020-03-10 14:01:56 +0000

    two way coms tested

--- pi_coms_2.py
+++ pi_coms_2.py
@@ -1,5 +1,6 @@
 import serial
 import json
+import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.flushInput()
@@ -11,7 +12,7 @@ def listenJSON():
         #print(readStr)
         readJson = json.loads(readStr)
         #print(readJson)
-        dist = readJson["range"]
+        dist = readJson["Colour"]
         imu = readJson["imu"]
         roll = imu[0]
         pitch = imu[1]
@@ -22,14 +23,16 @@ def listenJSON():
         pass
 
 #function to send data formatted as JSON doc over serial
-def writeJSON(int led):
-    sendStr = json.dumps({'LED': led}, separators=(',',':'))
-    ser.write(sendStr + "\n")
+def writeJSON(led):
+    sendStr = json.dumps({'LED':led}, separators=(',',':')) + "\n"
+    #print(sendStr)
+    ser.write(sendStr.encode('utf-8')) #has to be encoded into bits
 
 i=0
 while True:
     listenJSON()
     writeJSON(i)
-    i++
-    if i == 10:
+    i=i+1
+    if i == 8:
         i=0
+    time.sleep(0.25) #so it can be seen when debugging
